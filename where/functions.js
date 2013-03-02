3a47780c1059eb289303ca5ade2a61523789095b
Tara Kola <kolatara@gmail.com>
2013-03-02 12:22:08 -0500

    testing

--- where/functions.js
+++ where/functions.js
@@ -7,75 +7,55 @@ var myOptions = {
 	center: me,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
-			var map;
-			var marker;
-			var infowindow = new google.maps.InfoWindow();
-			var places;
+var map;
+var marker;
+var infowindow = new google.maps.InfoWindow();
+var places;
 
-			function init()
-			{
-				map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-				getMyLocation();
-			}
+function init()
+{
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	getMyLocation();
+}
 
-			function getMyLocation()
-			{
-				if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
-					navigator.geolocation.getCurrentPosition(function(position) {
-						myLat = position.coords.latitude;
-						myLng = position.coords.longitude;
-						renderMap();
-					});
-				}
-				else {
-					alert("Geolocation is not supported by your web browser.  What a shame!");
-				}
-			}
+function getMyLocation()
+{
+	if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+		navigator.geolocation.getCurrentPosition(function(position) {
+		myLat = position.coords.latitude;
+		myLng = position.coords.longitude;
+		renderMap();
+		});
+	}
+	else {
+		alert("Geolocation is not supported by your web browser.  What a shame!");
+	}
+}
 
-			function renderMap()
-			{
-				me = new google.maps.LatLng(myLat, myLng);
+function renderMap()
+{
+	me = new google.maps.LatLng(myLat, myLng);
 
 				// Update map and go there...
-				map.panTo(me);
+	map.panTo(me);
 
 				// Create a marker
-				marker = new google.maps.Marker({
-					position: me,
-					title: "Here I Am!"
-				});
-				marker.setMap(map);
+	marker = new google.maps.Marker({
+		position: me,
+		title: "Here I Am!"
+	});
+	marker.setMap(map);
 
 				// Open info window on click of marker
 				google.maps.event.addListener(marker, 'click', function() {
 					infowindow.setContent(marker.title);
 					infowindow.open(map, marker);
 				});
+}
 
-				// Calling Google Places API
-				var request = {
-					location: me,
-					radius: '500',
-					types: ['food']
-				};
-				service = new google.maps.places.PlacesService(map);
-				service.search(request, callback);
-			}
-
-			// Taken from http://code.google.com/apis/maps/documentation/javascript/places.html
-			function callback(results, status)
-			{
-				if (status == google.maps.places.PlacesServiceStatus.OK) {
-					alert("Got places back!");
-					places = results;
-					for (var i = 0; i < results.length; i++) {
-						createMarker(results[i]);
-					}
-				}
-			}
-
-			function createMarker(place)
-			{
+/*
+function createMarker(place)
+{
 				var placeLoc = place.geometry.location;
 				var marker = new google.maps.Marker({
 					map: map,
@@ -87,4 +67,4 @@ var myOptions = {
 					infowindow.setContent(place.name);
 					infowindow.open(map, this);
 				});
-      }
+      }*/
