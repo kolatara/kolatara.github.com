7640b723b9f5e434f5ecea00abd2d764d3f1a756
Tara Kola <kolatara@gmail.com>
2013-03-03 13:49:15 -0500

    test

--- where/functions.js
+++ where/functions.js
@@ -269,7 +269,7 @@ function calculateDistance(lat, lng)
 		Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 	var d = R * c / 1.6;
-	var output = new Number(dist);
+	var output = new Number(d);
 	return output.toPrecision(3);
 }
 
